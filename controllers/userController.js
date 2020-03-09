33de91b6a46a3c5fbd67d7808631a53b55f0a3a0
Tia Hwang <tiajshwang@gmail.com>
2020-03-09 16:19:45 +0900

    #7.0~#7.3 User Profile/Change Password/Adding Creator

--- controllers/userController.js
+++ controllers/userController.js
@@ -111,13 +111,57 @@ export const getMe = (req, res) => {
 };
 
 // User Detail
-export const userDetail = (req, res) =>
-    res.render("userDetail", { pageTitle: "User Detail" });
+export const userDetail = async (req, res) => {
+    const {
+        params: { id }
+    } = req;
+    try {
+        const user = await User.findById(id);
+        res.render("userDetail", { pageName: "User Detail", user });
+    } catch (error) {
+        res.redirect(routes.home);
+    }
+};
 
 // Edit Profile
-export const editProfile = (req, res) =>
+export const getEditProfile = (req, res) =>
     res.render("editProfile", { pageTitle: "Edit Profile" });
 
+export const postEditProfile = async (req, res) => {
+    const {
+        body: { name, email },
+        file
+    } = req;
+    try {
+        await User.findByIdAndUpdate(req.user.id, {
+            name,
+            email,
+            avatarUrl: file ? file.path : req.user.avatarUrl
+        });
+        res.redirect(routes.me);
+    } catch (error) {
+        res.redirect(routes.editProfile);
+    }
+};
+
 // Change Password
-export const changePassword = (req, res) =>
+export const getChangePassword = (req, res) =>
     res.render("changePassword", { pageTitle: "Change Password" });
+
+export const postChangePassword = async (req, res) => {
+    const {
+        body: { oldPassword, newPassword, newPassword1 }
+    } = req;
+    try {
+        if (newPassword !== newPassword1) {
+            res.status(400);
+            res.redirect(`/users${routes.changePassword}`);
+            return;
+        }
+        await req.user.changePassword(oldPassword, newPassword);
+        res.redirect(routes.me);
+    } catch (error) {
+        res.status(400);
+        res.redirect(`/users${routes.changePassword}`);
+    }
+};
